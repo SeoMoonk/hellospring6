164ebf9ed4dbdf8d4f689b1c84fb14470c5845bd
seomoon <sunghoon1216@naver.com>
2025-02-13 17:10:36 +0900

    환율을 ê api를 요청하고, 이를 읽어 JSON으로 변환 후 활용하여 서비스 완성

---
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -0,0 +1,61 @@
+package tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;                       //주문 번호
+    private String currency;                    //통화 종류
+    private BigDecimal foreignCurrencyAmount;   //외화 기준 가격
+    private BigDecimal exRate;                  //적용 환율
+    private BigDecimal convertedAmount;         //원화 환산 금액
+    private LocalDateTime validUntil;           //원화 환산 금액 유효 시간
+
+
+    //NOTE: 생성자 싧수를 줄이기 위한 빌더 패턴이나 Lombok
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
+                   BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
