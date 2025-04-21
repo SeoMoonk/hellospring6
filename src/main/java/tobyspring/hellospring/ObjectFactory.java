c604be6070508c8d488720dfbf8071e95d0a9ef4
unknown <sunghoon1216@skuniv.ac.kr>
2025-04-21 14:53:07 +0900

    refactor: BeanFactory를 í, DI 적용을 통한 스프링식 응집향상, 결합하향

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,13 +1,18 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
     //ObjectFactory 클래스의 이점 -> 예를 들어, exRateProvider를 변경하고 싶을 때, 바로 접근하여 변경이 가능하다. + 관심사 분리
-
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
