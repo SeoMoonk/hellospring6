ec6bc2d6bde2b91d905f677f7972821d6ad3fd00
unknown <sunghoon1216@skuniv.ac.kr>
2025-04-21 15:09:09 +0900

    refactor: bean config 보를 가져오는  또다른 방법 Component와 ComponentScan

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,12 +1,15 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
     //ObjectFactory 클래스의 이점 -> 예를 들어, exRateProvider를 변경하고 싶을 때, 바로 접근하여 변경이 가능하다. + 관심사 분리
-    @Bean
+/*    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
@@ -14,5 +17,5 @@ public class ObjectFactory {
     @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
-    }
+    }*/
 }
