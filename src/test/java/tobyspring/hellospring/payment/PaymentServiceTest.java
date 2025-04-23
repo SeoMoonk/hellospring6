969fd91fd8bdf6f377727f3a5745f579003e6474
seomoon <sunghoon1216@naver.com>
2025-04-23 15:39:40 +0900

    test: PaymentService 테스트 추가
    환율 정보를 받아오고, 금액과 유효시간이 잔 계산되는지 테스트

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -0,0 +1,34 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PaymentServiceTest {
+
+    @Test
+    @DisplayName("prepare 메서드가 요구사항 3가지를 잘 충족했는지 검증")
+    void prepare() throws IOException {
+        //given
+        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+
+        //when
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        //then
+        // 1. 환율 정보를 가져온다, (환율은 항시 변동되기에 notnull 테스트로 진행)
+        assertThat(payment.getExRate()).isNotNull();
+        // 2. 원화 환산 금액 계산
+        assertThat(payment.getConvertedAmount())
+                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+        // 3. 원화 환산금액 유효시간 계산
+        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+}
