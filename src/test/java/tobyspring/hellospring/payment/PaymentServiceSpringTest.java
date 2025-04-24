2a675e84aa6c350b273722d13a2c5172b6dbb409
seomoon <sunghoon1216@naver.com>
2025-04-24 12:26:17 +0900

    test: Spring DI 를 활용한 테스트 추가
    Dependency Injection 방식의 스프링식 PaymentService 테스트

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,42 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.TestObjectFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
+class PaymentServiceSpringTest {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Autowired
+    private ExRateProviderStub exRateProviderStub;
+
+    @Test
+    @DisplayName("prepare 메서드가 요구사항 3가지를 잘 충족했는지 검증")
+    void convertedAmount() throws IOException {
+        // exRate : 1000
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+
+        //exRate:500
+        exRateProviderStub.setExRate(BigDecimal.valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment2.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(5_000));
+    }
+}
