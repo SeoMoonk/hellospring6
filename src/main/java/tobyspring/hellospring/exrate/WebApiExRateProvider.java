45e1da87a986bb17dfed34f1f6cbeef0f353873c
seomoon <sunghoon1216@naver.com>
2025-05-02 13:01:43 +0900

    refactor: exrate from web refactoring by method extract

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -17,8 +17,6 @@ public class WebApiExRateProvider implements ExRateProvider {
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data;
         String response;
         URI uri;
 
@@ -29,22 +27,30 @@ public class WebApiExRateProvider implements ExRateProvider {
         }
 
         try {
-            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                //try-with-resource (자원의 사용이 끝나고 자동으로 자원(br)을 반납해줌) - auto closable
-                //만약 실행 도중 exception 으로 인해 튕겨 나가면서 제대로 반납을 안하면, 메모리 할당 문제가 발생할 수 있음)
-                response = br.lines().collect(Collectors.joining());
-            }
+            response = executeApi(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         try {
-            data = mapper.readValue(response, ExRateData.class);
-            return data.rates().get("KRW");
+            return parseExRate(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static BigDecimal parseExRate(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        return data.rates().get("KRW");
+    }
+
+    private String executeApi(URI uri) throws IOException {
+        String response;
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            return br.lines().collect(Collectors.joining());
         }
     }
 }
