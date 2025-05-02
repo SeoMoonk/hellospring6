54f53bf53ea2aa054aa69654849e23c7dc2c4849
seomoon <sunghoon1216@naver.com>
2025-05-02 12:50:37 +0900

    refactor: IOException 남발 제거, try-with-resource를통한 자원 반납

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,33 +1,50 @@
 package tobyspring.hellospring.exrate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.net.URI;
-import tobyspring.hellospring.payment.ExRateProvider;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
+import tobyspring.hellospring.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
 
     @Override
     public BigDecimal getExRate(String currency) {
-        URI uri = new URI("https://open.er-api.com/v6/latest/" + currency);
-
-        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
+        String url = "https://open.er-api.com/v6/latest/" + currency;
         ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
+        ExRateData data;
+        String response;
+        URI uri;
 
-        System.out.println("API ExRate : " + data.rates().get("KRW"));
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
-        return data.rates().get("KRW");
+        try {
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                //try-with-resource (자원의 사용이 끝나고 자동으로 자원(br)을 반납해줌) - auto closable
+                //만약 실행 도중 exception 으로 인해 튕겨 나가면서 제대로 반납을 안하면, 메모리 할당 문제가 발생할 수 있음)
+                response = br.lines().collect(Collectors.joining());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            data = mapper.readValue(response, ExRateData.class);
+            return data.rates().get("KRW");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
