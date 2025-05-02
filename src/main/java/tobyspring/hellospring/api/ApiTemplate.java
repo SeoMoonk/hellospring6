d7b981d433449c7991fdbd559ed0e4148df71bae
seomoon <sunghoon1216@naver.com>
2025-05-02 15:05:24 +0900

    refactor: api 호출 방식 변경, executor로 분리 + api 템플릿

---
+++ src/main/java/tobyspring/hellospring/api/ApiTemplate.java
@@ -0,0 +1,33 @@
+package tobyspring.hellospring.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ApiTemplate {
+
+    public BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        URI uri;
+        String response;
+
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            response = apiExecutor.execute(uri);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return exRateExtractor.extractExRate(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
