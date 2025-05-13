eb2b992281ba295a0a89adcec2f068e1539ab297
seomoon <sunghoon1216@naver.com>
2025-05-13 19:48:28 +0900

    feat: emf를 활용해 jpa repository의 동작원리 이해하기

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -1,9 +1,8 @@
 package tobyspring.hellospring;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,21 +10,11 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-        // em 만들기
-        EntityManager em = emf.createEntityManager();
-
-        // transaction -> commit
-        // em.persist (영속화 해주세요)
-        em.getTransaction().begin();
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
+        repository.save(order);
 
         System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
     }
 }
